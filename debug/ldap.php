d16384e95a9a217a2496c56c8930a1bc140ce5ac
fab1en <fab1en@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-05-11 15:00:29 +0000

    [FEATURE] Add debug script, resolves #26763
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ig_ldap_sso_auth/trunk@47579 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ debug/ldap.php
@@ -0,0 +1,43 @@
+<?php
+
+$basedn = ''; 
+$password = ''; 
+$server = "ldap://...";
+
+$username = 'username';  // Username to be search in the LDAP directory
+
+//using ldap bind anonymously // connect to ldap server 
+$ds = ldap_connect($server)
+	or die("Could not connect to LDAP server."); 
+ 
+ldap_set_option($ds, LDAP_OPT_PROTOCOL_VERSION, 3); 
+ 
+if ($ds) {
+	// binding anonymously
+	$ldapbind = ldap_bind($ds);
+	
+	if ($ldapbind) {
+		echo "LDAP bind anonymous successful...";
+	}
+	else{
+		echo "LDAP bind anonymous failed...";
+	}
+}
+
+// search in the directory
+$r = ldap_search( $ds, $basedn, 'uid=' . $username);
+
+
+if ($r) {
+	$result = ldap_get_entries( $ds, $r);
+	if ($result[0]) {
+		if (ldap_bind( $ds, $result[0]['dn'], $password)) {
+			#print_r('... successfully identified');
+			#print_r($result);
+			#print_r($result[0]);
+		}
+	}
+}
+
+
+?>

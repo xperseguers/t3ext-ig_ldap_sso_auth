0ee7247b44489210901855d0cb5ee2f069841367
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-05 15:14:23 +0000

    [BUGFIX] Fix syntax error in pi1 wizicon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ig_ldap_sso_auth/trunk@56048 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_igldapssoauth_pi1_wizicon.php
+++ pi1/class.tx_igldapssoauth_pi1_wizicon.php
@@ -69,7 +69,7 @@ class tx_igldapssoauth_pi1_wizicon {
 	}
 }
 
-if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ig_ldap_sso_auth/pi1/class.tx_igldapssoauth_pi1_wizicon.php'])) {
+if (defined('TYPO3_MODE') && isset($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ig_ldap_sso_auth/pi1/class.tx_igldapssoauth_pi1_wizicon.php'])) {
 	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/ig_ldap_sso_auth/pi1/class.tx_igldapssoauth_pi1_wizicon.php']);
 }
 

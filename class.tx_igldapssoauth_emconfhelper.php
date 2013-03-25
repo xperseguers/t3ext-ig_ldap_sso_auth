98fe8095a18853752eaccf4e9fae36bce1940440
mtremblay <mtremblay@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-03-25 15:50:28 +0000

    Configuration checker TYPO3 6.x compatibility
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ig_ldap_sso_auth/trunk@73716 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_igldapssoauth_emconfhelper.php
+++ class.tx_igldapssoauth_emconfhelper.php
@@ -27,7 +27,7 @@
  * ************************************************************* */
 // Make sure that we are executed only in TYPO3 context
 if (!defined('TYPO3_MODE'))
-	die('Access denied.');
+    die('Access denied.');
 
 /**
  * class providing configuration checks for saltedpasswords.
@@ -38,160 +38,127 @@ if (!defined('TYPO3_MODE'))
  * @package	TYPO3
  * @subpackage	ig_ldap_sso_auth
  */
-class tx_igldapssoauth_emconfhelper {
+class tx_igldapssoauth_emconfhelper extends \TYPO3\CMS\Core\TypoScript\ConfigurationForm {
 
-	/**
-	 * @var	integer
-	 */
-	protected $errorType = t3lib_FlashMessage::OK;
-	/**
-	 * @var	string
-	 */
-	protected $header;
-	/**
-	 * @var	string
-	 */
-	protected $preText;
+    /**
+     * @var	integer
+     */
+    protected $errorType = t3lib_FlashMessage::OK;
 
-	/*
-	 * @var	array
-	 */
-	protected $problems = array();
+    /**
+     * @var	string
+     */
+    protected $header;
 
-	/**
-	 * Set the error level if no higher level
-	 * is set already
-	 *
-	 * @param	string		$level: one out of error, ok, warning, info
-	 * @return	void
-	 */
-	private function setErrorLevel($level) {
+    /**
+     * @var	string
+     */
+    protected $preText;
 
-		switch ($level) {
-			case 'error':
-				$this->errorType = t3lib_FlashMessage::ERROR;
-				$this->header = 'Errors found in your configuration';
-				$this->preText = '<br />';
-				break;
-			case 'warning':
-				if ($this->errorType < t3lib_FlashMessage::ERROR) {
-					$this->errorType = t3lib_FlashMessage::WARNING;
-					$this->header = 'Warnings about your configuration';
-					$this->preText = '<br />';
-				}
-				break;
-			case 'info':
-				if ($this->errorType < t3lib_FlashMessage::WARNING) {
-					$this->errorType = t3lib_FlashMessage::INFO;
-					$this->header = 'Additional information';
-					$this->preText = '<br />';
-				}
-				break;
-			case 'ok':
-				// TODO: Remove INFO condition as it has lower importance
-				if ($this->errorType < t3lib_FlashMessage::WARNING && $this->errorType != t3lib_FlashMessage::INFO) {
-					$this->errorType = t3lib_FlashMessage::OK;
-					$this->header = 'No errors were found';
-					$this->preText = 'Configuration has been configured correctly.<br />';
-				}
-				break;
-		}
-	}
+    /*
+     * @var	array
+     */
+    protected $problems = array();
 
-	/**
-	 * Renders the flash messages if problems have been found.
-	 *
-	 * @return	string		The flash message as HTML.
-	 */
-	private function renderFlashMessage() {
-		$message = '';
-		// if there are problems, render them into an unordered list
-		if (count($this->problems) > 0) {
-			$message = <<< EOT
-<ul>
-	<li>###PROBLEMS###</li>
-</ul>
-EOT;
-			$message = str_replace('###PROBLEMS###', implode('<br />&nbsp;</li><li>', $this->problems), $message);
-		}
+    /**
+     * Checks the backend configuration and shows a message if necessary.
+     *
+     * @param	array				$params: Field information to be rendered
+     * @param	t3lib_tsStyleConfig	$pObj: The calling parent object.
+     * @return	string				Messages as HTML if something needs to be reported
+     */
+    public function checkConfiguration() {
+        $this->extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ig_ldap_sso_auth']);
 
-		if (empty($message)) {
-			$this->setErrorLevel('ok');
-		}
+        // Configuration of authentication service.
+        $loginSecurityLevel = $GLOBALS['TYPO3_CONF_VARS']['BE']['loginSecurityLevel'];
 
-		$message = $this->preText . $message;
-		$flashMessage = t3lib_div::makeInstance('t3lib_FlashMessage', $message, $this->header, $this->errorType);
-
-		return $flashMessage->render();
-	}
-
-	/**
-	 * Initializes this object.
-	 *
-	 * @return void
-	 */
-	private function init() {
-		$requestSetup = $this->processPostData((array) $_REQUEST['data']);
-		$this->extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['ig_ldap_sso_auth']);
-	}
-
-	/**
-	 * Checks the backend configuration and shows a message if necessary.
-	 *
-	 * @param	array				$params: Field information to be rendered
-	 * @param	t3lib_tsStyleConfig	$pObj: The calling parent object.
-	 * @return	string				Messages as HTML if something needs to be reported
-	 */
-	public function checkConfiguration(array $params, t3lib_tsStyleConfig $pObj) {
-		$this->init();
-
-		// Configuration of authentication service.
-		$loginSecurityLevel = $GLOBALS['TYPO3_CONF_VARS']['BE']['loginSecurityLevel'];
-
-		if ($loginSecurityLevel == 'challenged' || $loginSecurityLevel == 'superchallenged' || $loginSecurityLevel == '' ) {
-			$this->setErrorLevel('error');
-			$problems[] = <<< EOT
+        if ($loginSecurityLevel == 'challenged' || $loginSecurityLevel == 'superchallenged' || $loginSecurityLevel == '') {
+            $this->setErrorLevel('error');
+            $problems[] = <<< EOT
 LDAPauthentification is not compatible with loginSecurityLevel set to "challenged" or "superchallenged" since the real password can never be sent against the LDAP repository. 
 Value of loginSecurityLevel should be handled manually to "normal" or even better "rsa" in the Install Tool. <br/><br/>
 
 	\$TYPO3_CONF_VARS['BE']['loginSecurityLevel'] = 'normal';<br/>
 	\$TYPO3_CONF_VARS['BE']['loginSecurityLevel'] = 'rsa';<br/>
 EOT;
-			$problems[] = 'Current value is : "' . $loginSecurityLevel . '"';
-		} else {
-			$this->setErrorLevel('ok');
-			$problems = array();
-		}
+            $problems[] = 'Current value is : "' . $loginSecurityLevel . '"';
+        } else {
+            $this->setErrorLevel('ok');
+            $problems = array();
+        }
 
-		$this->problems = $problems;
+        $this->problems = $problems;
 
-		return $this->renderFlashMessage();
-	}
+        return $this->renderFlashMessage();
+    }
 
-	/**
-	 * Processes the information submitted by the user using a POST request and
-	 * transforms it to a TypoScript node notation.
-	 *
-	 * @param	array		$postArray: Incoming POST information
-	 * @return	array		Processed and transformed POST information
-	 */
-	private function processPostData(array $postArray = array()) {
-		foreach ($postArray as $key => $value) {
-			// TODO: Explain
-			$parts = explode('.', $key, 2);
+    /**
+     * Set the error level if no higher level
+     * is set already
+     *
+     * @param	string		$level: one out of error, ok, warning, info
+     * @return	void
+     */
+    private function setErrorLevel($level) {
 
-			if (count($parts) == 2) {
-				// TODO: Explain
-				$value = $this->processPostData(array($parts[1] => $value));
-				$postArray[$parts[0] . '.'] = array_merge((array) $postArray[$parts[0] . '.'], $value);
-			} else {
-				// TODO: Explain
-				$postArray[$parts[0]] = $value;
-			}
-		}
+        switch ($level) {
+            case 'error':
+                $this->errorType = t3lib_FlashMessage::ERROR;
+                $this->header = 'Errors found in your configuration';
+                $this->preText = '<br />';
+                break;
+            case 'warning':
+                if ($this->errorType < t3lib_FlashMessage::ERROR) {
+                    $this->errorType = t3lib_FlashMessage::WARNING;
+                    $this->header = 'Warnings about your configuration';
+                    $this->preText = '<br />';
+                }
+                break;
+            case 'info':
+                if ($this->errorType < t3lib_FlashMessage::WARNING) {
+                    $this->errorType = t3lib_FlashMessage::INFO;
+                    $this->header = 'Additional information';
+                    $this->preText = '<br />';
+                }
+                break;
+            case 'ok':
+                // TODO: Remove INFO condition as it has lower importance
+                if ($this->errorType < t3lib_FlashMessage::WARNING && $this->errorType != t3lib_FlashMessage::INFO) {
+                    $this->errorType = t3lib_FlashMessage::OK;
+                    $this->header = 'No errors were found';
+                    $this->preText = 'Configuration has been configured correctly.<br />';
+                }
+                break;
+        }
+    }
 
-		return $postArray;
-	}
+    /**
+     * Renders the flash messages if problems have been found.
+     *
+     * @return	string		The flash message as HTML.
+     */
+    private function renderFlashMessage() {
+        $message = '';
+        // if there are problems, render them into an unordered list
+        if (count($this->problems) > 0) {
+            $message = <<< EOT
+<ul>
+	<li>###PROBLEMS###</li>
+</ul>
+EOT;
+            $message = str_replace('###PROBLEMS###', implode('<br />&nbsp;</li><li>', $this->problems), $message);
+        }
+
+        if (empty($message)) {
+            $this->setErrorLevel('ok');
+        }
+
+        $message = $this->preText . $message;
+        $flashMessage = t3lib_div::makeInstance('t3lib_FlashMessage', $message, $this->header, $this->errorType);
+
+        return $flashMessage->render();
+    }
 
 }
 

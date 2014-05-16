bf99342629fe6651a76d6966fa2382daee6c0c78
Xavier Perseguers <xavier@typo3.org>
2014-05-16 10:33:04 +0200

    [BUGFIX] Deprecated assignment already in PHP 5.2
    Change-Id: I8a488610b71ff61b67bcddf7fba2395601782b63
    Reviewed-on: https://review.typo3.org/30164
    Reviewed-by: Xavier Perseguers
    Tested-by: Xavier Perseguers

--- Classes/CAS/client.php
+++ Classes/CAS/client.php
@@ -1364,7 +1364,7 @@ class CASClient
       }
 
       // create the storage object
-      $this->_pgt_storage = &new PGTStorageFile($this,$format,$path);
+      $this->_pgt_storage = new PGTStorageFile($this,$format,$path);
     }
 
   /**
@@ -1401,7 +1401,7 @@ class CASClient
       trigger_error('PGT storage into database is an experimental feature, use at your own risk',E_USER_WARNING);
 
       // create the storage object
-      $this->_pgt_storage = & new PGTStorageDB($this,$user,$password,$database_type,$hostname,$port,$database,$table);
+      $this->_pgt_storage = new PGTStorageDB($this,$user,$password,$database_type,$hostname,$port,$database,$table);
     }
 
   // ########################################################################

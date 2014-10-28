2bc215d5d9dcd87f6c15b4b84e93a79ca491dd63
Xavier Perseguers <xavier@typo3.org>
2014-10-28 13:23:55 +0100

    [!!!][TASK] Deprecate support for PHP 5.2
    Releases: 2.0
    Resolves: #62516
    Change-Id: I4d61d5acccb1c4c3b7521a7206ddb5dd25f50e98
    Reviewed-on: http://review.typo3.org/33593
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-6.2.99',
-			'php' => '5.2.0-5.6.99',
+			'php' => '5.3.3-5.6.99',
 		),
 		'conflicts' => array(
 		),

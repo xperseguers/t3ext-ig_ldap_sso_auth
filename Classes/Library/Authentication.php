92b8879183d386399304a07cf0fab4cf58bb3b09
Xavier Perseguers <xavier@typo3.org>
2015-03-26 18:55:03 +0100

    [BUGFIX] New users cannot be automatically created
    Releases: master, 2.1
    Fixes: #66055
    Change-Id: I2e17b90ef484f95bb13972c915c51e967558465a
    Reviewed-on: http://review.typo3.org/38262
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- Classes/Library/Authentication.php
+++ Classes/Library/Authentication.php
@@ -667,7 +667,7 @@ class Authentication {
 				}
 
 				// If field exists in TYPO3, set it to the mapped value
-				if (isset($typo3[$field])) {
+				if (array_key_exists($field, $typo3)) {
 					$typo3[$field] = $mappedValue;
 
 				// Otherwise, it is some extra value, which we store in a special sub-array

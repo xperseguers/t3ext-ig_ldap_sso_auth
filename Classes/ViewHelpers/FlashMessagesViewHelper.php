b702b79e2559fcd7b660456e7390c03fe93e274d
Xavier Perseguers <xavier@typo3.org>
2017-04-25 14:17:12 +0200

    [BUGFIX] Ensure FlashMessageViewHelper is compatible with PHP < 7.0
    Change-Id: I9af36a8b73c0d880fbdfaa32b1a87ffe9ea93ddf
    Reviewed-on: https://review.typo3.org/52599
    Reviewed-by: Xavier Perseguers <xavier@typo3.org>
    Tested-by: Xavier Perseguers <xavier@typo3.org>

--- Classes/ViewHelpers/FlashMessagesViewHelper.php
+++ Classes/ViewHelpers/FlashMessagesViewHelper.php
@@ -14,6 +14,8 @@
 
 namespace Causal\IgLdapSsoAuth\ViewHelpers;
 
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
 /**
  * Render a conf* View helper which renders the flash messages (if there are any).
  *
@@ -22,79 +24,8 @@ namespace Causal\IgLdapSsoAuth\ViewHelpers;
  * @subpackage ig_ldap_sso_auth
  */
 if (version_compare(TYPO3_branch, '8', '>=')) {
-    class FlashMessagesViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\FlashMessagesViewHelper
-    {
-        /**
-         * Renders the flash messages as unordered list
-         *
-         * @param array $flashMessages \TYPO3\CMS\Core\Messaging\FlashMessage[]
-         * @return string
-         * @typo3 8
-         */
-        protected function renderDefault(array $flashMessages) : string
-        {
-            $flashMessagesClass = $this->hasArgument('class') ? $this->arguments['class'] : 'typo3-messages';
-            $content = sprintf('<div class="%s">', $flashMessagesClass);
-            /** @var \TYPO3\CMS\Core\Messaging\FlashMessage $singleFlashMessage */
-            foreach ($flashMessages as $singleFlashMessage) {
-                $content .= $this->renderFlashMessage($singleFlashMessage);
-            }
-            $content .= '</div>';
-            return $content;
-        }
-
-        /**
-         * @param \TYPO3\CMS\Core\Messaging\FlashMessage $flashMessage
-         * @return string
-         * @see \TYPO3\CMS\Core\Messaging\FlashMessage::getMessageAsMarkup
-         */
-        protected function renderFlashMessage(\TYPO3\CMS\Core\Messaging\FlashMessage $flashMessage)
-        {
-            $messageTitle = $flashMessage->getTitle();
-            $markup = [];
-            $markup[] = '<div class="alert ' . htmlspecialchars($flashMessage->getClass()) . '">';
-            $markup[] = '    <div class="media">';
-            $markup[] = '        <div class="media-left">';
-            $markup[] = '            <span class="fa-stack fa-lg">';
-            $markup[] = '                <i class="fa fa-circle fa-stack-2x"></i>';
-            $markup[] = '                <i class="fa fa-' . htmlspecialchars($flashMessage->getIconName()) . ' fa-stack-1x"></i>';
-            $markup[] = '            </span>';
-            $markup[] = '        </div>';
-            $markup[] = '        <div class="media-body">';
-            if (!empty($messageTitle)) {
-                $markup[] = '            <h4 class="alert-title">' . htmlspecialchars($messageTitle) . '</h4>';
-            }
-            $markup[] = '            <p class="alert-message">' . $flashMessage->getMessage() . '</p>';
-            $markup[] = '        </div>';
-            $markup[] = '    </div>';
-            $markup[] = '</div>';
-            return implode('', $markup);
-        }
-
-        /**
-         * Renders the flash messages as unordered list
-         *
-         * @param array $flashMessages \TYPO3\CMS\Core\Messaging\FlashMessage[]
-         * @return string
-
-         */
-        protected function renderAsList(array $flashMessages)
-        {
-            $flashMessagesClass = $this->hasArgument('class') ? $this->arguments['class'] : 'typo3-messages';
-            $content = sprintf('<ul class="%s">', $flashMessagesClass);
-            /** @var \TYPO3\CMS\Core\Messaging\FlashMessage $singleFlashMessage */
-            foreach ($flashMessages as $singleFlashMessage) {
-                $severityClass = sprintf('alert %s', $singleFlashMessage->getClass());
-                $messageContent = $singleFlashMessage->getMessage();
-                if ($singleFlashMessage->getTitle() !== '') {
-                    $messageContent = sprintf('<h4>%s</h4>', htmlspecialchars($singleFlashMessage->getTitle())) . $messageContent;
-                }
-                $content .= sprintf('<li class="%s">%s</li>', htmlspecialchars($severityClass), $messageContent);
-            }
-            $content .= '</ul>';
-            return $content;
-        }
-    }
+    // This file requires PHP >= 7
+    include(ExtensionManagementUtility::extPath('ig_ldap_sso_auth') . 'Resources/Private/Php/FlashMessageViewHelper.v8.php');
 } else {
     class FlashMessagesViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\FlashMessagesViewHelper
     {

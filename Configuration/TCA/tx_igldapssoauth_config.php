4b28aa1ff24fd1c0f9f8b9cff24411e1bd0e9a54
Xavier Perseguers <xavier@causal.ch>
2019-03-25 14:04:53 +0100

    [TASK] Migrate deprecated references to EXT:lang labels

--- Configuration/TCA/tx_igldapssoauth_config.php
+++ Configuration/TCA/tx_igldapssoauth_config.php
@@ -58,7 +58,7 @@ return [
     'columns' => [
         'hidden' => [
             'exclude' => 1,
-            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.hidden',
+            'label' => 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
             'config' => [
                 'type' => 'check',
                 'default' => '0'

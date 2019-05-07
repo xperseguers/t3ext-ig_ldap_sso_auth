f987e70a6c926642edc84eb87bd9b72e89bffba9
oheil <git@heilbit.de>
2019-05-07 14:43:11 +0200

    static call Random::generateRandomBytes deprecated

--- Classes/Domain/Repository/Typo3UserRepository.php
+++ Classes/Domain/Repository/Typo3UserRepository.php
@@ -427,7 +427,8 @@ class Typo3UserRepository
         if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('saltedpasswords')) {
             $instance = \TYPO3\CMS\Saltedpasswords\Salt\SaltFactory::getSaltingInstance(null, TYPO3_MODE);
         }
-        $password = Random::generateRandomBytes(16);
+        //$password = Random::generateRandomBytes(16);
+        $password = GeneralUtility::makeInstance(Random::class)->generateRandomBytes(16);
         $password = $instance ? $instance->getHashedPassword($password) : md5($password);
         return $password;
     }

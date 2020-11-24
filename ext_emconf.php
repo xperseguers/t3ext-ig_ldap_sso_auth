f746dbe18c8dcecb9b25661ae554cc6202e4fa74
Xavier Perseguers <xavier@causal.ch>
2020-11-24 06:55:58 +0100

    [TASK] Remove deprecated and removed $EM_CONF options

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,24 +15,14 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'This extension provides LDAP support for TYPO3 by delegating the authentication of frontend and/or backend users to the centrally-managed directory of your organization. It fully supports OpenLDAP and Active Directory and is capable of connecting securely to the authentication server using either TLS or SSL (ldaps://).
 In case of use in an intranet environment, this extension is a perfect match since it natively brings Single Sign-On (SSO) capability to TYPO3 without any complex configuration.',
     'category' => 'services',
-    'shy' => 0,
     'version' => '3.6.0',
-    'dependencies' => '',
-    'conflicts' => '',
-    'priority' => '',
-    'loadOrder' => '',
-    'module' => '',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
-    'modify_tables' => 'be_groups,be_users,fe_groups,fe_users',
     'clearcacheonload' => 0,
-    'lockType' => '',
     'author' => 'Xavier Perseguers',
     'author_email' => 'xavier@causal.ch',
     'author_company' => '',
-    'CGLcompliance' => '',
-    'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-7.4.99',
@@ -41,8 +31,6 @@ In case of use in an intranet environment, this extension is a perfect match sin
         'conflicts' => [],
         'suggests' => [],
     ],
-    '_md5_values_when_last_written' => '',
-    'suggests' => [],
     'autoload' => [
         'psr-4' => ['Causal\\IgLdapSsoAuth\\' => 'Classes']
     ],

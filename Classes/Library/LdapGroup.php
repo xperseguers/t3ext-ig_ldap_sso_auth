9f7c4b542acb52fe6ec854e8eb171136368a303c
Thomas Off <t.off@datamints.com>
2023-04-17 10:08:21 +0200

    [TASK] Merge calls to str_replace() for better performance; reformat code.

--- Classes/Library/LdapGroup.php
+++ Classes/Library/LdapGroup.php
@@ -26,100 +26,121 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 class LdapGroup
 {
 
-    /**
-     * Returns LDAP group records based on a list of DNs provided as $membership,
-     * taking group's baseDN and filter into consideration.
-     *
-     * @param array $membership
-     * @param string $baseDn
-     * @param string $filter
-     * @param array $attributes
-     * @param bool $extendedCheck true if groups should be actively checked against LDAP server, false to check against baseDN solely
-     * @param \Causal\IgLdapSsoAuth\Library\Ldap $ldapInstance
-     * @return array
-     */
-    public static function selectFromMembership(array $membership = [], $baseDn, $filter, array $attributes = [], $extendedCheck = true, \Causal\IgLdapSsoAuth\Library\Ldap $ldapInstance = null)
-    {
-        $ldapGroups['count'] = 0;
+	/**
+	 * Returns LDAP group records based on a list of DNs provided as $membership,
+	 * taking group's baseDN and filter into consideration.
+	 *
+	 * @param array $membership
+	 * @param string $baseDn
+	 * @param string $filter
+	 * @param array $attributes
+	 * @param bool $extendedCheck true if groups should be actively checked against LDAP server, false to check against baseDN solely
+	 * @param \Causal\IgLdapSsoAuth\Library\Ldap $ldapInstance
+	 * @return array
+	 */
+	public static function selectFromMembership(
+		array $membership = [],
+		$baseDn,
+		$filter,
+		array $attributes = [],
+		$extendedCheck = true,
+		\Causal\IgLdapSsoAuth\Library\Ldap $ldapInstance = null
+	) {
+		$ldapGroups['count'] = 0;
 
-        if (count($membership) === 0 || empty($filter) || $ldapInstance === null) {
-            return $ldapGroups;
-        }
+		if (count($membership) === 0 || empty($filter) || $ldapInstance === null) {
+			return $ldapGroups;
+		}
 
-        unset($membership['count']);
+		unset($membership['count']);
 
-        foreach ($membership as $groupDn) {
-            if (!empty($baseDn) && strcasecmp(substr($groupDn, -strlen($baseDn)), $baseDn) !== 0) {
-                // Group $groupDn does not match the required baseDn for LDAP groups
-                continue;
-            }
-            if ($extendedCheck) {
-                $ldapGroup = $ldapInstance->search($groupDn, $filter, $attributes);
-            } else {
-                $parts = explode(',', $groupDn);
-                list($firstAttribute, $value) = explode('=', $parts[0]);
-                $firstAttribute = strtolower($firstAttribute);
-                $ldapGroup = [
-                    0 => [
-                        0 => $firstAttribute,
-                        $firstAttribute => [
-                            0 => $value,
-                            'count' => 1,
-                        ],
-                        'dn' => $groupDn,
-                        'count' => 1,
-                    ],
-                    'count' => 1,
-                ];
-            }
-            if (!isset($ldapGroup['count']) || $ldapGroup['count'] == 0) {
-                continue;
-            }
-            $ldapGroups['count']++;
-            $ldapGroups[] = $ldapGroup[0];
-        }
+		foreach ($membership as $groupDn) {
+			if (!empty($baseDn) && strcasecmp(substr($groupDn, -strlen($baseDn)), $baseDn) !== 0) {
+				// Group $groupDn does not match the required baseDn for LDAP groups
+				continue;
+			}
+			if ($extendedCheck) {
+				$ldapGroup = $ldapInstance->search($groupDn, $filter, $attributes);
+			} else {
+				$parts = explode(',', $groupDn);
+				list($firstAttribute, $value) = explode('=', $parts[0]);
+				$firstAttribute = strtolower($firstAttribute);
+				$ldapGroup = [
+					0 => [
+						0 => $firstAttribute,
+						$firstAttribute => [
+							0 => $value,
+							'count' => 1,
+						],
+						'dn' => $groupDn,
+						'count' => 1,
+					],
+					'count' => 1,
+				];
+			}
+			if (!isset($ldapGroup['count']) || $ldapGroup['count'] == 0) {
+				continue;
+			}
+			$ldapGroups['count']++;
+			$ldapGroups[] = $ldapGroup[0];
+		}
 
-        return $ldapGroups;
-    }
+		return $ldapGroups;
+	}
 
-    /**
-     * Returns groups associated to a given user (identified either by his DN or his uid attribute).
-     *
-     * @param string $baseDn
-     * @param string $filter
-     * @param string $userDn
-     * @param string $userUid
-     * @param array $attributes
-     * @param \Causal\IgLdapSsoAuth\Library\Ldap $ldapInstance
-     * @return array
-     */
-    public static function selectFromUser($baseDn, $filter = '', $userDn = '', $userUid = '', array $attributes = [], \Causal\IgLdapSsoAuth\Library\Ldap $ldapInstance = null)
-    {
-        if ($ldapInstance === null) {
-            return [];
-        }
+	/**
+	 * Returns groups associated to a given user (identified either by his DN or his uid attribute).
+	 *
+	 * @param string $baseDn
+	 * @param string $filter
+	 * @param string $userDn
+	 * @param string $userUid
+	 * @param array $attributes
+	 * @param \Causal\IgLdapSsoAuth\Library\Ldap $ldapInstance
+	 * @return array
+	 */
+	public static function selectFromUser(
+		$baseDn,
+		$filter = '',
+		$userDn = '',
+		$userUid = '',
+		array $attributes = [],
+		\Causal\IgLdapSsoAuth\Library\Ldap $ldapInstance = null
+	) {
+		if ($ldapInstance === null) {
+			return [];
+		}
 
-        $filter = str_replace('{USERDN}', $ldapInstance->escapeDnForFilter($userDn), $filter);
-        $filter = str_replace('{USERUID}', $ldapInstance->escapeDnForFilter($userUid), $filter);
+		$filter = str_replace(
+			[
+				'{USERDN}',
+				'{USERUID}'
+			],
+			[
+				$ldapInstance->escapeDnForFilter($userDn),
+				$ldapInstance->escapeDnForFilter($userUid)
+			],
+			$filter
+		);
 
-        $groups = $ldapInstance->search($baseDn, $filter, $attributes);
-        return $groups;
-    }
+		$groups = $ldapInstance->search($baseDn, $filter, $attributes);
+		return $groups;
+	}
 
-    /**
-     * Returns the membership information for a given user.
-     *
-     * @param array $ldapUser
-     * @param array $mapping
-     * @return array|bool
-     */
-    public static function getMembership(array $ldapUser = [], array $mapping = [])
-    {
-        if (isset($mapping['usergroup']) && preg_match("`<([^$]*)>`", $mapping['usergroup'], $attribute)) {
-            return $ldapUser[strtolower($attribute[1])];
-        }
+	/**
+	 * Returns the membership information for a given user.
+	 *
+	 * @param array $ldapUser
+	 * @param array $mapping
+	 * @return array|bool
+	 */
+	public static function getMembership(array $ldapUser = [], array $mapping = [])
+	{
+		if (isset($mapping['usergroup']) && preg_match("`<([^$]*)>`", $mapping['usergroup'], $attribute)) {
+			return $ldapUser[strtolower($attribute[1])];
+		}
 
-        return false;
-    }
+		return false;
+	}
 
 }

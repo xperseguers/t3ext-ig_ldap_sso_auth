26baff2aa051094a1563aa7a76613c2cc98e3d27
Philipp <github@philipp-sessner.de>
2024-01-13 13:44:13 +0100

    Fix return in ImportUsers::setConfiguration

--- Classes/Task/ImportUsers.php
+++ Classes/Task/ImportUsers.php
@@ -305,6 +305,7 @@ class ImportUsers extends \TYPO3\CMS\Scheduler\Task\AbstractTask
     public function setConfiguration(int $configuration): self
     {
         $this->configuration = $configuration;
+        return $this;
     }
 
     /**

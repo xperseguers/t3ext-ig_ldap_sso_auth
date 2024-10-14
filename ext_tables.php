38ff6a6c33f49d7f98543a7455239c1a699c0802
Xavier Perseguers <xavier@causal.ch>
2024-10-14 14:16:00 +0200

    [TASK] Migrate registration of additional sprite icons

--- ext_tables.php
+++ ext_tables.php
@@ -2,17 +2,6 @@
 defined('TYPO3') || die();
 
 (static function (string $_EXTKEY) {
-    // Register additional sprite icons
-    /** @var \TYPO3\CMS\Core\Imaging\IconRegistry $iconRegistry */
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon('extensions-ig_ldap_sso_auth-overlay-ldap-record',
-        \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-        [
-            'source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/overlay-ldap-record.png',
-        ]
-    );
-    unset($iconRegistry);
-
     $typo3Version = (new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion();
     if ($typo3Version < 12) {
         // Add BE module on top of system main module

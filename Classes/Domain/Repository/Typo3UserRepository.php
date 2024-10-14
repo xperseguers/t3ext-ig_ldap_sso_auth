2cd92e9ba72e82dd04f5636ca062b8f2a364ffde
Adrian Rudnik <adrian@klonmaschine.de>
2024-10-14 11:36:03 +0200

    [BUGFIX] Forcing control TCA enablecolumns "disable" property to int typed representation

--- Classes/Domain/Repository/Typo3UserRepository.php
+++ Classes/Domain/Repository/Typo3UserRepository.php
@@ -179,8 +179,10 @@ class Typo3UserRepository
 
         $tableConnection = GeneralUtility::makeInstance(ConnectionPool::class)
             ->getConnectionForTable($table);
-        // tstamp needs to be int. If it's empty it's transferred as ""
+        // tstamp and disable needs to be int. If it's empty it's transferred as ""
         $data['tstamp'] = (int)$data['tstamp'];
+        $data['disable'] = (int)$data['disable'];
+
         $tableConnection->insert(
             $table,
             $data
@@ -228,8 +230,9 @@ class Typo3UserRepository
 
         $cleanData = $data;
         unset($cleanData['__extraData']);
-        // tstamp needs to be int. If it's empty it's transferred as ""
+        // tstamp and disable needs to be int. If it's empty it's transferred as ""
         $cleanData['tstamp'] = (int)$cleanData['tstamp'];
+        $cleanData['disable'] = (int)$cleanData['disable'];
 
         $affectedRows = GeneralUtility::makeInstance(ConnectionPool::class)
             ->getConnectionForTable($table)
